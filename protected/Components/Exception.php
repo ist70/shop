3a4c92b55c4756ddcb90908cfe1edd81ccf74e2e
Vitaliy <ist70@mail.ru>
2015-09-15 11:03:02 +0400

    Начало

--- protected/Components/Exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Components;
-
-class Exception
-    extends \T4\Core\Exception
-{
-
-} 
--- protected/Migrations/m_1439713105_Contact_CreateContacts.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Migrations;
-
-class m_1439713105_Contact_CreateContacts
-    extends \App\Modules\Contact\Migrations\m_1424204358_CreateContacts
-{
-
-}
